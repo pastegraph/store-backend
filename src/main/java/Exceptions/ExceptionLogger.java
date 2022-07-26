ca1e209f724e1471c0c4d9a53bc525b6ed8eeb8d
pashabezborod <paul.bezborodov@yandex.ru>
2022-07-26 22:35:50 +0500

    Обновил логгер

--- src/main/java/Exceptions/ExceptionLogger.java
+++ src/main/java/Exceptions/ExceptionLogger.java
@@ -1,9 +1,30 @@
 package Exceptions;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ExceptionLogger {
 
-    public static void log(Throwable e) {
-        System.err.println(e.getMessage());
-        e.printStackTrace(System.err);
+    static Path logPath = Path.of(System.getProperty("user.home") + File.separator + "pastegraphLogs.txt");
+
+    public static synchronized void log(Throwable e) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd h:mm");
+        StringBuilder log = new StringBuilder();
+        log.append("\n**********\n");
+        log.append(simpleDateFormat.format(new Date())).append("\n").append(e.getMessage());
+        for (StackTraceElement temp : e.getStackTrace()) {
+            log.append(temp.toString()).append("\n");
+        }
+
+        try {
+            Files.writeString(logPath, log, StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
